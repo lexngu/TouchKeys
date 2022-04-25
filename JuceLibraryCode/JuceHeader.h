03cacd04a8d13bad16ae017828bcb0665129e76c
Alexander Nguyen <alexander.nguyen@stud.hfm-karlsruhe.de>
2022-04-25 21:07:37 +0200

    Use JUCE v6.1.6; macOS only

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -10,8 +10,7 @@
 
 */
 
-#ifndef __APPHEADERFILE_IJU11L__
-#define __APPHEADERFILE_IJU11L__
+#pragma once
 
 #include "AppConfig.h"
 
@@ -28,6 +27,15 @@
 
 #include "BinaryData.h"
 
+#if defined (JUCE_PROJUCER_VERSION) && JUCE_PROJUCER_VERSION < JUCE_VERSION
+ /** If you've hit this error then the version of the Projucer that was used to generate this project is
+     older than the version of the JUCE modules being included. To fix this error, re-save your project
+     using the latest version of the Projucer or, if you aren't using the Projucer to manage your project,
+     remove the JUCE_PROJUCER_VERSION define.
+ */
+ #error "This project was last saved using an outdated version of the Projucer! Re-save this project with the latest version to fix this error."
+#endif
+
 #if ! DONT_SET_USING_JUCE_NAMESPACE
  // If your code uses a lot of JUCE classes, then this will obviously save you
  // a lot of typing, but can be disabled by setting DONT_SET_USING_JUCE_NAMESPACE.
@@ -38,9 +46,8 @@
 namespace ProjectInfo
 {
     const char* const  projectName    = "TouchKeys";
+    const char* const  companyName    = "Queen Mary, University of London";
     const char* const  versionString  = "0.1.0";
     const int          versionNumber  = 0x100;
 }
 #endif
-
-#endif   // __APPHEADERFILE_IJU11L__
